199bf6ac732dc92cbe484b51863e442f1b522bd7
hgat <hgat@hotmail.co.uk>
2012-07-08 11:15:09 +0100

    Fixed

--- src/incl/connect.php
+++ src/incl/connect.php
@@ -1,18 +1,15 @@
 <?php
-	$conn_error = 'Could not connect to the database';
-	
-	$mysql_host = 'localhost';
-	$mysql_user = 'root';
-	$mysql_pass = '';
+require 'settings.php'; //loads our $mysql_ vars (as well as a lot of unneeded stuff)
 
-	$mysql_database = 'jupu';
+$conn_error = 'Could not connect to the database';
 
-	if(@!mysql_connect($mysql_host, $mysql_user, $mysql_pass)||!mysql_select_db($mysql_database)){
-		$mysql_host = 'jupucouk.ipagemysql.com';
-		$mysql_user = 'hgat';
-		$mysql_pass = 'haruhI!';
-		if(!mysql_connect($mysql_host, $mysql_user, $mysql_pass)||!mysql_select_db($mysql_database)){
-			die($conn_error);
-		}
-	}
+if(@!mysql_connect($mysql_host, $mysql_user, $mysql_pass)||!mysql_select_db($mysql_database)){
+
+if(!mysql_connect($mysql_host, $mysql_user, $mysql_pass)||!mysql_select_db($mysql_database)){
+
+die($connerror);
+
+}
+
+}
 ?>
